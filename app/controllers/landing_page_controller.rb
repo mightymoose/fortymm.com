5502d4e72e840765fa7c9feabae782e51f3234e8
Ryan Morris <mryan.morris@gmail.com>
2024-11-30 19:34:18 -0600

    feat: :sparkles: redirect from both the landing and dashboard pages

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,7 @@
 class LandingPageController < ApplicationController
   def index
+    if current_user
+      redirect_to dashboard_path
+    end
   end
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:one)
     get dashboard_index_url
     assert_response :success
   end
