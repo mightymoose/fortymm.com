6aba534002b2c99763074bfc16bc28ddb307e234
Ryan Morris <mryan.morris@gmail.com>
2024-12-10 18:56:32 -0600

    chore: :fire: remove the phoenix boilerplate on the landing page

--- test/fortymm_web/controllers/page_controller_test.exs
+++ test/fortymm_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FortymmWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "FortyMM"
   end
 end
